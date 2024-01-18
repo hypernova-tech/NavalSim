4f69971a9e1b9695e8b2db9d8e25113f7a56a855
hypernovatech <subscription@hypernova-tech.com>
2024-01-18 22:48:24 +0300

    termal material

---
+++ Source/BoatSim/Private/Lib/Thermal/ThermalMaterialManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Lib/ActorBase/ActorBase.h"
+#include "ThermalMaterialManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AThermalMaterialManager : public AActorBase
+{
+	GENERATED_BODY()
+	
+};
